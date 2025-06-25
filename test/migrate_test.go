c11f4928e7b5c3f2f18fd692784ebe2e1c7ad76d
pix <tpxsky@163.com>
2025-06-25 22:23:08 +0800

    <add:>Add all

---
+++ test/migrate_test.go
@@ -0,0 +1,15 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func MigrateTest(t *testing.T) {
+}
+
+func Migrate(ctx context.Context) error {
+	fmt.Println("test migrate")
+	return nil
+}
